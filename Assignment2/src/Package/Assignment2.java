95a5a9f9859bc7eb7f721f738e43c91373072b8d
kejifasuyi <43154234+kejifasuyi@users.noreply.github.com>
2018-10-16 01:01:40 -0300

    Assignment2

--- Assignment2/src/Package/Assignment2.java
+++ Assignment2/src/Package/Assignment2.java
@@ -1,54 +1,26 @@
 package Package;
 
-import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.swing.JOptionPane;
 
-public class Entrance {
+public class Assignment2 {
 
 	public static void main(String[] args) {
 		System.setProperty("java.util.logging.config.file", "./logging.properties");
 		int user_input;
-		MySQLAccess dao = new MySQLAccess();
 		Transaction t = new Transaction();
-		Operations operations = new Operations();
-		Ops o = new Ops();
+		Operations o = new Operations();
 
 		user_input = Integer.valueOf(JOptionPane.showInputDialog("What transaction would you like to perform today?\n"
 				+ "1. Create New \n2. Update \n3. Delete \n4. View"));
 
 		if (user_input == 1) {
-			try {
-				Connection connections = dao.setupConnection();
-				t = operations.makeTransaction();
-				o.createTransaction(t);
-
-				if (connections != null) {
-					connections.close();
-				}
-
-			} catch (Exception e) {
-				Logger.getLogger("Main").log(Level.WARNING, e.getLocalizedMessage().toString());
-				e.printStackTrace();
-			}
-
+			o.createTransaction(t);
 		}
 
 		else if (user_input == 2) {
-			try {
-				Connection connections = dao.setupConnection();
-				o.updateTransaction(t);
-
-				if (connections != null) {
-					connections.close();
-				}
-
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			o.updateTransaction(t);
 		}
 
 		else if (user_input == 3) {
